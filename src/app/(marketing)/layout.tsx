463568351b58d7071d2f13b56c819040bd71a1fa
MarwanBz <kaka.mer1998@gmail.com>
2025-01-06 22:00:05 +0300

    initial commit

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <div>
+        <h1>Marketing Layout</h1>
+      </div>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default MarketingLayout;
